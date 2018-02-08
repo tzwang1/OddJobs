c98e93d1ed2a5f8c1ccf85b77ea8500123e86c31
Truman Wang <truman.wang@mail.utoronto.ca>
2018-02-08 00:32:36 -0500

    Added job board to jobs.html

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
+// Functions for job.html
+
+// Shows the form to submit a new job
 function show_new_job_text_box() {
 	document.getElementById("new_job_sub_box").style.display = "block";
+	document.getElementById("job_board").style.display = "none";
 }
 
+// Posts a new job
 function submit_new_job(){
 	var job_title = document.getElementById("job_title").value;
 	var job_description = document.getElementById("job_description").value;
@@ -10,6 +15,15 @@ function submit_new_job(){
 	console.log(job_description);
 }
 
+// Shows the job board
+function show_job_board() {
+	document.getElementById("job_board").style.display = "block";
+	document.getElementById("new_job_sub_box").style.display = "none";
+}
+
+// Functions for login.html
+
+// Verifies username and password, when prompted to log in
 function log_in(){
 	var username = document.getElementById("login").value;
 	var password = document.getElementById("password").value;
