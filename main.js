880dc2015d6b6441f57e844a835e8bf77b1a8037
Truman Wang <truman.wang@mail.utoronto.ca>
2018-02-07 22:54:05 -0500

    adding some job submit functionality

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
-function show_drop_down() {
-	document.getElementById("job_dropdown").classList.toggle("show");
+function show_new_job_text_box() {
+	document.getElementById("new_job_sub_box").style.display = "block";
+}
+
+function submit_new_job(){
+	var job_title = document.getElementById("job_title");
+	var job_description = document.getElementById("job_description");
+
+	console.log(job_title);
+	console.log(job_description);
 }
