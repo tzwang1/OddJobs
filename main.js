4ea975869d1c05e1283a93d0f77a5a851d396220
Truman Wang <truman.wang@mail.utoronto.ca>
2018-02-07 23:32:08 -0500

    adding some form for login and password

--- main.js
+++ main.js
@@ -3,9 +3,17 @@ function show_new_job_text_box() {
 }
 
 function submit_new_job(){
-	var job_title = document.getElementById("job_title");
-	var job_description = document.getElementById("job_description");
+	var job_title = document.getElementById("job_title").value;
+	var job_description = document.getElementById("job_description").value;
 
 	console.log(job_title);
 	console.log(job_description);
+}
+
+function log_in(){
+	var username = document.getElementById("login").value;
+	var password = document.getElementById("password").value;
+
+	console.log(username);
+	console.log(password);
 }
