2383da6fea75c222d6e18f2c40e1cd3013c843e7
Truman Wang <tzwang@Trumans-MacBook-Pro-2.local>
2019-03-18 10:33:25 -0400

    Restructuring files

---
+++ backend/design_doc.md
@@ -0,0 +1,25 @@
+# Design Doc
+
+## Use case
+OddJobs will provide a platform for posting simple jobs such as mowing a lawn, shoveling snow, etc and allow users to sign up to take on these jobs.
+
+## Features
+### User login / registration
+### Posting a Job
+### Accepting a Job
+### Searching for a Job
+### Viewing a profile
+### Rating
+
+## Technologies
+### Frontend
+React + Redux
+
+### Backend
+MongoDB <br>
+Python + Flask <br>
+Docker + Kubernetes <br>
+
+
+
+
---
+++ backend/tests/test_app.py
@@ -0,0 +1,45 @@
+import os
+import sys
+import pytest
+
+from pymongo import MongoClient
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from app import app
+
+@pytest.fixture
+def test_db():
+    test_db = MongoClient('localhost', 27017)
+    return test_db['test_db']
+
+@pytest.fixture
+def odd_jobs_app():
+    tester = app.test_client()
+    return tester
+
+@pytest.fixture
+def user_data():
+    user = {'username': 'Tom', 'password': '123', 'email': 'tom.pleb@gmail.com'}
+    return user
+
+def test_db_connection(test_db):
+    assert test_db != None
+
+def test_index(odd_jobs_app):
+    response = odd_jobs_app.get('/')
+    assert response.status_code == 200
+    assert response.data == b'Hello World!'
+
+def test_create_user(odd_jobs_app, user_data):
+    response = odd_jobs_app.post('/user', data=user_data)
+    assert response.status_code == 200
+    assert response.data == b'User was created'
+
+
+
+
+
+
+
+
+
