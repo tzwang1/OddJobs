45600e68ab2a27cb775680788cb8cb200eeaab34
tzwang1 <truman.wang1234@gmail.com>
2019-03-17 20:48:34 -0400

    Creating API endpoint to create a user.

--- backend/Authentication/app.py
+++ backend/Authentication/app.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
     return 'Hello World!'
+
+@app.route('/user/<id>', methods=['GET'])
+def get_user(id):
+    return 'User has id: '.format(id)
+
+@app.route('/user', methods=['POST'])
+def post_user():
+    return 'User was created'
 
 if __name__=="__main__":
     app.run(debug=True, host='0.0.0.0', port=80)
--- backend/Authentication/tests/test-app.py
+++ backend/Authentication/tests/test-app.py
@@ -5,7 +5,6 @@ import pytest
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from app import app
 
-
 @pytest.fixture
 def auth_app():
     tester = app.test_client()
@@ -15,6 +14,11 @@ def test_index(auth_app):
     response = auth_app.get('/')
     assert response.status_code == 200
     assert response.data == b'Hello World!'
+
+def test_create_user(auth_app):
+    response = auth_app.post('/user')
+    assert response.status_code == 200
+    assert response.data == b'User was created'
 
 
 
